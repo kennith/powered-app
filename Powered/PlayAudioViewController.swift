5ef5a2162d4c4f37d78dc4a3abd27cd679307fcc
Kennith Leung <kennith.leung@gmail.com>
2014-11-23 20:22:41 -0800

    finish session 3

--- Powered/PlayAudioViewController.swift
+++ Powered/PlayAudioViewController.swift
@@ -7,6 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayAudioViewController: UIViewController {
+	var audioPlayer: AVAudioPlayer!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+			var filePathURL = NSURL(fileURLWithPath: filePath)
+			audioPlayer = AVAudioPlayer(contentsOfURL: filePathURL, error: nil)
+		} else {
+			println("File not found")
+		}
+//		audioPlayer.play()
+	}
+	
+	func playAudio(rate:float_t) {
+		audioPlayer.stop()
+		audioPlayer.currentTime = 0.0
+		audioPlayer.enableRate = true
+		audioPlayer.rate = rate
+		audioPlayer.play()
+	}
+	
+	@IBAction func PlaySlowAction(sender: UIButton) {
+		playAudio(0.5)
+	}
+	
+	@IBAction func PlayFastAction(sender: UIButton) {
+		playAudio(2.0)
+	}
+	
+	@IBAction func StopAction(sender: UIButton) {
+		audioPlayer.stop()
+	}
 }
